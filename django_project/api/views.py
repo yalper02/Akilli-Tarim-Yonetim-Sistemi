ca9e4472023d54b151c466357162d5f82eea196e
Yiğit Alper Ayhan <250542014@firat.edu.tr>
2026-06-03 17:07:32 +0300

    Proje dokümanı eklendi

--- django_project/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-
-def sensor_data(request):
-    return JsonResponse({
-        "sicaklik": 28,
-        "nem": 45
-    })
--- django_project/mqtt_demo.py
+++
@@ -1,8 +0,0 @@
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-client.connect("broker.hivemq.com", 1883)
-
-client.publish("tarim/nem", "Toprak Nemi: 45")
-
-print("MQTT mesaj gönderildi")
